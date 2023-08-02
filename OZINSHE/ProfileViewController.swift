77fe5fdd763a0326e676e987b9ce8fddbcc18c6e
alibekt1012 <alibekt1012@gmail.com>
2023-08-02 12:11:14 +0600

    - Add translations for profile view controller label and buttons - Then add translated data into profile view controller

--- OZINSHE/ProfileViewController.swift
+++ OZINSHE/ProfileViewController.swift
@@ -13,6 +13,12 @@ class ProfileViewController: UIViewController, LanguageProtocol {
     @IBOutlet var myProfileLabel: UILabel!
     @IBOutlet var languageButton: UIButton!
     @IBOutlet var languageLabel: UILabel!
+    @IBOutlet var personalDataButton: UIButton!
+    @IBOutlet var personalDataLabel: UILabel!
+    @IBOutlet var termsAndConditionsButton: UIButton!
+    @IBOutlet var changePasswordButton: UIButton!
+    @IBOutlet var notificationsButton: UIButton!
+    @IBOutlet var darkModeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,15 +31,25 @@ class ProfileViewController: UIViewController, LanguageProtocol {
         
         myProfileLabel.text = "MY_PROFILE".localized()
         languageButton.setTitle("LANGUAGE".localized(), for: .normal)
+        personalDataButton.setTitle("PERSONAL_DATA".localized(), for: .normal)
+        changePasswordButton.setTitle("CHANGE_PASSWORD".localized(), for: .normal)
+        termsAndConditionsButton.setTitle("TERMS_AND_CONDITIONS".localized(), for: .normal)
+        notificationsButton.setTitle("NOTIFICATIONS".localized(), for: .normal)
+        darkModeButton.setTitle("DARK_MODE".localized(), for: .normal)
+        
+        
         
         if Localize.currentLanguage() == "ru" {
             languageLabel.text = "Русский"
+            personalDataLabel.text = "Править"
         }
         if Localize.currentLanguage() == "kk" {
             languageLabel.text = "Қазақша"
+            personalDataLabel.text = "Өңдеу"
         }
         if Localize.currentLanguage() == "en" {
             languageLabel.text = "English"
+            personalDataLabel.text = "Edit"
         }
         
     }
